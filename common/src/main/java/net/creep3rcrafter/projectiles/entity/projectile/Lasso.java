2b7773aa3663ff3b110085dbfc5be2b0a6beea15
Brenden Delano <brendendelano@gmail.com>
2024-02-29 18:59:16 -0700

    1.0.0

--- common/src/main/java/net/creep3rcrafter/projectiles/entity/projectile/Lasso.java
+++ common/src/main/java/net/creep3rcrafter/projectiles/entity/projectile/Lasso.java
@@ -12,10 +12,8 @@ import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.LeadItem;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.EntityHitResult;
-import net.minecraft.world.phys.HitResult;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
